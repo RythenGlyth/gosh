be79f6d6072c5cccf1cbcb075ec97c392802c3cf
Lenni <lennivh24@gmail.com>
2021-02-22 15:55:02 +0000

    using the new gosh package

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module gosh
 go 1.15
 
 require (
-	github.com/scrouthtv/termios v0.0.0-20210216072851-8201aeffb6f6
-	github.com/scrouthtv/termios/utf8 v0.0.0-20210216125743-1b0887406147
+	github.com/scrouthtv/termios v0.0.0-20210222150922-9390d11380ae
+	github.com/scrouthtv/termios/keys v0.0.0-20210216185427-dee6bbf46613
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -2,90 +2,20 @@ package main
 
 import (
 	"os"
-	"fmt"
 
-	"gosh/prompt"
-
-	"github.com/scrouthtv/termios"
-	"github.com/scrouthtv/termios/utf8"
+	"gosh/gosh"
 )
 
-var MyGosh *Gosh
-
-type Gosh struct {
-	term termios.Terminal
-	p *prompt.Prompt
-	ready bool
-}
-
-func NewGosh() (*Gosh) {
-	return &Gosh{nil, nil, false}
-}
-
-func (g *Gosh) Init() error {
-	var err error
-	var term termios.Terminal
-	term, err = termios.Open()
-	if err != nil {
-		return err
-	}
-	err = term.SetRaw(true)
-	if err != nil {
-		return err
-	}
-	g.term = term
-
-	g.p = prompt.NewPrompt(term)
-	return nil
-}
-
-func (g *Gosh) Close() {
-	g.ready = false
-	if (g.term != nil) {
-		g.term.Close()
-	}
-}
+var MyGosh *gosh.Gosh
 
 func main() {
-	var retcode int = 0
-	defer func() { os.Exit(retcode) }()
 
-	MyGosh = NewGosh()
+	MyGosh = gosh.NewGosh()
 
-	var err error
-	err = MyGosh.Init()
+	retcode, err := MyGosh.Interactive()
 	if err != nil {
-		os.Stdout.WriteString("Error occured during intialization:")
 		os.Stdout.WriteString(err.Error())
-		os.Stdout.WriteString("\n")
-		retcode = 1
-		return
-	}
-	defer MyGosh.Close()
-
-	var n int
-	var buf []byte = make([]byte, 1024)
-	var key utf8.Key
-	var keys []utf8.Key
-
-	MyGosh.term.Write([]byte(fmt.Sprintf("This is %s %s. Press C-d to exit.\r\n", GoshName, GoshVersion)))
-
-	for {
-		n, err = MyGosh.term.Read(buf)
-		if err != nil {
-			// Consider MyGosh.term broken:
-			os.Stdout.WriteString("Error reading input\n")
-		} else {
-			if buf[0] == 0x04 { // C-d to quit
-				retcode = 0
-				return
-			} else {
-				keys = utf8.ParseUTF8(buf[:n])
-				for _, key = range keys {
-					MyGosh.p.OnKey(key)
-				}
-			}
-		}
 	}
 
+	os.Exit(retcode)
 }
