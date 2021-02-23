4531070239ed076397fe97c96a3d20efcb80c524
Lenni <lennivh24@gmail.com>
2021-02-23 14:43:49 +0000

    wrapping debug errors

---
+++ src/debug/debugError.go
@@ -0,0 +1,27 @@
+package debug
+
+// LaunchError is returned if the debugger could not start
+type LaunchError struct {
+	err error
+}
+
+func (e *LaunchError) Unwrap() error {
+	return e.err
+}
+
+func (e *LaunchError) Error() string {
+	return "error starting debugger: " + e.err.Error()
+}
+
+// WriteError is returned if an error occured whilst writing debugging data
+type WriteError struct {
+	err error
+}
+
+func (e *WriteError) Unwrap() error {
+	return e.err
+}
+
+func (e *WriteError) Error() string {
+	return "error writing debugging data: " + e.err.Error()
+}
