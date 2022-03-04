d2588d13f00d2141dbaa801aa49e72158afde451
Lenni <lennivh24@gmail.com>
2022-03-04 10:49:59 +0100

    tests: gosh stub for testing purposes

---
+++ src/test_util/gosh_stub.go
@@ -0,0 +1,26 @@
+package test_util
+
+import (
+	"gosh/src/shared"
+	"testing"
+)
+
+type GoshStub struct {
+	T *testing.T
+}
+
+func (g *GoshStub) ChangeWD(string) error                { return nil }
+func (g *GoshStub) Close()                               {}
+func (g *GoshStub) GetAliasManager() shared.AliasManager { return nil }
+
+func (g *GoshStub) GetDebugger() shared.IDebugger                { return nil }
+func (g *GoshStub) SetDebugger(shared.IDebugger)                 {}
+func (g *GoshStub) DebugMessage(shared.ModuleIdentifier, string) {}
+
+func (g *GoshStub) GetEventHandler() shared.IEventHandler   { return nil }
+func (g *GoshStub) GetPluginManager() shared.IPluginManager { return nil }
+func (g *GoshStub) GetWD() (string, error)                  { return "", nil }
+func (g *GoshStub) Init() error                             { return nil }
+func (g *GoshStub) Interactive() (int, error)               { return 0, nil }
+func (g *GoshStub) RegisterBuiltin(shared.Builtin)          {}
+func (g *GoshStub) WriteString(s string) (int, error)       { g.T.Log(s); return 0, nil }
